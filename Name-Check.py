414d0cf04998c7fe68b071b842120af7c21fc422
Muhammad Omer <Mhamad.omer2002@gmail.com>
2023-06-25 00:18:36 +0300

    Update Name-Check.py
    Added a quit option.
    fixed some of the messages for more clarity.

--- Name-Check.py
+++ Name-Check.py
@@ -2,25 +2,28 @@ while True:
 
     original = []
     reverse = []
-    naww = input("enter name: ")
+    naww = input("\nEnter a word: ")
     aksiNaw = naww[::-1]
 
     for i in naww:
         original.append(i)
     for ii in aksiNaw:
         reverse.append(ii)
-        
-    if naww == aksiNaw:
-        print("you are correct")
+    if naww=="q":
+        print("\nThank you for using Palindrome Test. Goodbye!\n")
+        break   
+    elif naww == aksiNaw:
+        print("The word is Palindrome.")
     elif naww.casefold() == aksiNaw.casefold():
-        print("needs correction")
+        print("The word is Palindrome if we ignore the uppercase.")
     
 
     else:
         if original[1:] == reverse[:-1]:
-            print("First letter wrong")
+            print("First letter out and it's Palindrome")
         
         elif original[:-1] == reverse[1:]:
-            print("last letter is wrong")
-        print("end of line")
+            print("last letter out and it's Palindrome")
+        else:
+            print("The word isn't Palindrome.")
 
